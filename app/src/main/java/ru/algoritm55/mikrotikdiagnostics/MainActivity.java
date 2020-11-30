b4db34a53b43e5916ac38ee58bf684be3a966823
xandrman <vdcUwN5N>
2020-12-01 00:19:48 +0600

    Added mikrotik-3.0.7.jar API library. Added ShowDataActivity.

--- app/src/main/java/ru/algoritm55/mikrotikdiagnostics/MainActivity.java
+++ app/src/main/java/ru/algoritm55/mikrotikdiagnostics/MainActivity.java
@@ -2,13 +2,36 @@ package ru.algoritm55.mikrotikdiagnostics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String IP_MESSAGE = "IP";
+    public static final String LOGIN_MESSAGE = "login";
+    public static final String PASSWORD_MESSAGE = "password";
+
+    TextInputLayout editTextIP, editTextLogin, editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextIP = findViewById(R.id.outlinedTextFieldIP);
+        editTextLogin = findViewById(R.id.outlinedTextFieldLogin);
+        editTextPassword = findViewById(R.id.outlinedTextFieldPassword);
+        editTextIP.requestFocus();
+    }
+
+    public void openShowDataActivity(View view) {
+        Intent intent = new Intent(this, ShowDataActivity.class);
+        intent.putExtra(IP_MESSAGE, editTextIP.getEditText().getText().toString());
+        intent.putExtra(LOGIN_MESSAGE, editTextLogin.getEditText().getText().toString());
+        intent.putExtra(PASSWORD_MESSAGE, editTextPassword.getEditText().getText().toString());
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/ru/algoritm55/mikrotikdiagnostics/ShowDataActivity.java
@@ -0,0 +1,14 @@
+package ru.algoritm55.mikrotikdiagnostics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_data);
+    }
+}
